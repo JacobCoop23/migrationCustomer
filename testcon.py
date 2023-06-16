d3455e6d11b303e1bd76430a2b92a83abd2d4506
JacobCoop23 <136328075+JacobCoop23@users.noreply.github.com>
2023-06-16 09:09:12 -0400

    Create testcon.py
    swissre addition to the code

---
+++ testcon.py
@@ -0,0 +1,9 @@
+import re
+import os
+import pandas as pd
+import numpy as np
+
+csvname = "C:\\Users\\Jacob\\migrfolder\\usersdata.csv"
+link_migr = 'migrationKraken.edifplatformlkoiu.swissre.com '
+
+#swissrecode to be added down here as appeared on the original files#
